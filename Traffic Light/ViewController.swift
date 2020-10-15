a742b65a096875f05ed84dd833cddf9789632121
Safonova Svetlana <Safonova_s@icloud.com>
2020-10-15 10:51:53 +0300

    светофор

--- Traffic Light/ViewController.swift
+++ Traffic Light/ViewController.swift
@@ -8,12 +8,33 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var redLight: UIView!
+    @IBOutlet var orangeLight: UIView!
+    
+    @IBOutlet var greenLight: UIView!
+    
+    @IBOutlet var button: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        redLight.layer.cornerRadius = 50
+        orangeLight.layer.cornerRadius = 50
+        greenLight.layer.cornerRadius = 50
     }
 
-
+    @IBAction func pushButton() {
+       
+        if redLight.alpha == 1 && orangeLight.alpha == 0.3 &&
+            greenLight.alpha == 0.3 {
+            redLight.alpha = 0.3
+            orangeLight.alpha = 1
+        }
+        if orangeLight.alpha == 1 && redLight.alpha == 0.3 && greenLight.alpha == 0.3 {
+            orangeLight.alpha = 0.3
+            greenLight.alpha = 1
+        }
+        redLight.alpha = 1
+        button.setTitle("Next", for: .normal)
+    }
+    
 }
 
